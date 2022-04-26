b55fd60cbfd6cea115faebce4ccc510f0d2b181e
doislike <ehdlsgur99@naver.com>
2022-04-26 18:10:32 +0900

    add urllib, test url text

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 import sys
+import urllib.request
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+
 from bs4 import BeautifulSoup
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("이누공")
-        self.setGeometry(300, 300, 400, 400)
+        self.setGeometry(300, 100, 800, 800)
         self.setWindowIcon(QIcon("icon.png"))
 
 app = QApplication(sys.argv)
@@ -18,9 +20,9 @@ window.show()
 
 app.exec_()
 
-url = 'http://www.tukorea.ac.kr/gopage/main/gonomalnotice.jsp?siteGubun=1&bbsConfigFK=357&pkid=195753&menuGubun=1'
-response = requests.get(url)
+url = "http://www.tukorea.ac.kr/front/boardlist.do?bbsConfigFK=1&siteGubun=14&menuGubun=1"
+html = urllib.request.urlopen(url).read()
+bsObject = BeautifulSoup(html, "html.parser")
+for span in bsObject.find_all('span', {'class': 'text'}):
+     print(span.text)
 
-print(response) # 응답 형식은 <Response [200]>
-print(response.status_code) # 응답 코드는 200 (정상 응답)
-print(response.text)    # 응답 내용 출력 (html, 엄청 긺)
